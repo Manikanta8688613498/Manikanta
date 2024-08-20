1961fd58c149f6f8e310deb4a116284ec0be1c16
Manikanta Kondaveti <manikantakondaveti2060@gmail.com>
2024-08-20 15:19:46 +0530

    commit

--- src/Mani.java
+++ src/Mani.java
@@ -1,9 +1,15 @@
-public class Mani{
-    public static void main(String[] args) {
+public class Mani
+{
+    public static void main(String args[])
+    {
         int a=10;
-        int b=20;
-        int c=a+b;
-        System.out.println();
+        int b=-10;
+        boolean c=true;
+        boolean d=false;
+        System.out.println(~a);//-11 (minus of total positive value which starts from 0)
+        System.out.println(~b);//9 (positive of total minus, positive starts from 0)
+        System.out.println(!c);//false (opposite of boolean value)
+        System.out.println(!d);//true
     }
 }
 
