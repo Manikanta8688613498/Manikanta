a9eec092a7bb4761c33038461d504eda83c5763f
Manikanta Kondaveti <manikantakondaveti2060@gmail.com>
2024-08-22 13:01:00 +0530

    commit

--- src/New.java
+++ src/New.java
@@ -1,62 +1,11 @@
 public class New {
     public static void main(String[] args)
     {
-                int a= 10;
-                int b=13;
-                int i;
-                for (i=0;i<=10;i++)
-                {
-                    System.out.println("i = :" +(i));
-                }
-                if (a>b) {
-                    System.out.println("a is big");
-                }
-                    else{
-                        System.out.println("b is big");
-                }
-
-                System.out.println("a==b is:" +(a==b));
-                System.out.println("a!=b is:"+(a!=b));
-                System.out.println(a<b);
-                System.out.println(a>b);
-                System.out.println(b++ + ++b);
-                System.out.println(a<=b);
-                System.out.println(a<<2);
-                System.out.println(a>>2);
-                System.out.println("hello");
-        int num;
-        String name;
-
-        // Assigning values
-        num = 10;
-        name = "MANIKANTA";
-
-        // Displaying the assigned values
-        System.out.println("num is assigned: " + num);
-        System.out.println("name is assigned: " + name);
-        System.out.println(a+=a);
-        System.out.println(b-=a);
-        System.out.println(a*=b);
-        System.out.print(b/=a);
-        System.out.println(a%=a);
-        int c=20;
-        System.out.println(a>b||a<c);
-        System.out.println(a>b|a<c);
-        System.out.println(a>b||a++<c);
-        System.out.println(a);
-        System.out.println(a>b|a++<c);
-        System.out.println(a);
-        int s=10;
-        float f=(int)s;
-        System.out.println(f);
-        System.out.println(s);
-        float w=22.04f;
-        int k=(int)w;
-        System.out.println(k);
-        System.out.println(w);
-        char j='A';
-        int q=(int)j;
-        System.out.println(q);
+        int i;
+        for (i=0;i<=128;i++)
+        {
+            System.out.println(i+ ":" +(char)i);
+        }
     }
 }
 
