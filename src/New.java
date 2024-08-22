7eb3273d9f8d4cf98bdea3119fc5d3c90cf685ef
Manikanta Kondaveti <manikantakondaveti2060@gmail.com>
2024-08-22 15:27:20 +0530

    commit

--- src/New.java
+++ src/New.java
@@ -1,11 +1,31 @@
 public class New {
     public static void main(String[] args)
     {
-        int i;
-        for (i=0;i<=128;i++)
-        {
-            System.out.println(i+ ":" +(char)i);
+        String originalString = "  Hello, World!  ";
+
+        String trimmedString = originalString.trim();
+        System.out.println("Trimmed String: '" + trimmedString + "'");
+
+        String upperCaseString = trimmedString.toUpperCase();
+        System.out.println("Uppercase String: '" + upperCaseString + "'");
+
+        String replacedString = upperCaseString.replace("WORLD", "UNIVERSE");
+        System.out.println("Replaced String: '" + replacedString + "'");
+
+        boolean startsWithHello = replacedString.startsWith("HELLO");
+        System.out.println("Starts with 'HELLO': " + startsWithHello);
+
+        String[] words = replacedString.split(", ");
+        System.out.println("Words:");
+        for (String word : words) {
+            System.out.println(word);
         }
+
+        String joinedString = String.join(", ", words);
+        System.out.println("Joined String: '" + joinedString + "'");
+
+        boolean containsUniverse = joinedString.contains("UNIVERSE");
+        System.out.println("Contains 'UNIVERSE': " + containsUniverse);
     }
 }
 
